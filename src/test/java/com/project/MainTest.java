e952816f96411ea9624efd6adef07d27aa481bb9
Emerson Millard <emillard.nt@gmail.com>
2022-06-09 19:47:57 -0700

    Initial commit

---
+++ src/test/java/com/project/MainTest.java
@@ -0,0 +1,16 @@
+package com.project;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MainTest
+{
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+}
